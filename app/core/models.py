764f21325ad2a2d600e6a08481d965fcb21b7b0c
Masha <maria@herolo.co.il>
2020-05-31 15:13:12 +0300

    create user model

--- app/core/models.py
+++ app/core/models.py
@@ -12,14 +12,13 @@ class UserManager(BaseUserManager):
 
         return user
 
-
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
-        return  user
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
@@ -13,7 +14,6 @@ class ModelTests(TestCase):
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-
 
     def test_new_user_normalized(self):
         email = 'test@TEST.COM'
